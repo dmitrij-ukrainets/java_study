483bc2187624fa58e17b6408ede8140517274c25
dmitrij-ukrainets <dmitrij_ukrainets@epam.com>
2017-03-23 12:04:37 +0300

    Variables

---
+++ src/VariablesClass.java
@@ -0,0 +1,30 @@
+
+public class VariablesClass {
+
+	public static void main(String[] args) {
+		int intNumber = 333;
+		short shortNumber = 222;
+		long longNumber = 7945;
+		
+		double doubleNumber = 786.55;
+		float floatNumber = 545.56f;
+		
+		char charMine = 'z';
+		boolean boolieanValue = true;
+		
+		byte byteValue = 127;
+		
+		System.out.println(intNumber);
+		System.out.println(shortNumber);
+		System.out.println(longNumber);
+		
+		System.out.println(doubleNumber);
+		System.out.println(floatNumber);
+		
+		System.out.println(charMine);
+		System.out.println(boolieanValue);
+		
+		System.out.println(byteValue);
+	}
+
+}
