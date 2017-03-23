bcdf6b9748ddd2e3798cbc850c7d43e63bd95da0
dmitrij-ukrainets <dmitrij_ukrainets@epam.com>
2017-03-23 12:05:40 +0300

    Loop

---
+++ src/LoopClass.java
@@ -0,0 +1,15 @@
+
+public class LoopClass {
+	public static void main(String[] args) {
+		int intValue = 0;
+		//boolean loop = intValue < 10;
+		String spase = " ";
+		
+		while (intValue < 10)
+		{
+			System.out.println("Correct" + spase + "value " + intValue);
+			intValue = intValue + 1;
+		}
+
+	}
+}
