06ea3e477ebeea071e6fb930b128ee07fa1010a5
dmitrij-ukrainets <dmitrij_ukrainets@epam.com>
2017-03-23 12:05:11 +0300

    Strings

---
+++ src/StringClass.java
@@ -0,0 +1,18 @@
+
+public class StringClass {
+	public static void main(String[] args) {
+	//String begin = "String";
+	//String first = " ";
+	//String second = "combination";
+	String end = ".";
+	//String phrase = begin + first + second + end;
+	//System.out.println(phrase);
+	
+	//System.out.println("Another" + " " + "phrase" + ".");
+	
+	byte lowerByte = -128;
+	byte upperByte = 127;
+	String universalWord = "value";
+	System.out.println("Byte " + universalWord + " can " + "be " + "from " + lowerByte + " to " + upperByte + end);
+	}
+}
